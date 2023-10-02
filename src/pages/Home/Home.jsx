3311cecef29b8c85668b4def983e6f429546c439
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-10-02 22:45:06 +0600

    layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 const Home = () => {
     return (
@@ -7,6 +9,17 @@ const Home = () => {
             <Header></Header>
             <Navbar></Navbar>
             <div className="text-3xl font-poppins font-bold">This is Homepage</div>
+            <div>
+                <div>
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div>
+                    <h2 className="text-4xl">News coming soon...</h2>
+                </div>
+                <div>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
