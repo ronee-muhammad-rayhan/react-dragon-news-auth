21e8843186c31a6927b6ea5a6624fd53ddb66844
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-10-03 10:03:35 +0600

    categories fetched and shown

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch('./categories.json')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setCategories(data);
+            });
+    }, []);
     return (
         <div>
-            <h2 className="text-2xl">Left Side</h2>
+            <h2 className="text-2xl">All Categories</h2>
+            {/* <p>{categories.length}</p> */}
+            {
+                categories.map(category => <Link className="block" key={category.id} to={`/catagory/${category.name}`}>{category.name}</Link>)
+            }
+
         </div>
     );
 };
