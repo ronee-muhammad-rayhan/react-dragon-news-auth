1ce4f73eee51c44bc97b1ce342f5d66234779922
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-10-03 09:01:45 +0600

    navbar modified

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,9 @@
-import img from '../../../assets/logo.png'
+import logo from '../../../assets/logo.png'
 import moment from 'moment';
 const Header = () => {
     return (
         <div className='text-center'>
-            <img className='mx-auto' src={img} alt="" />
+            <img className='mx-auto' src={logo} alt="" />
             <p>Journalism Without Fear or Favour</p>
             <p className='text-xl'>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
